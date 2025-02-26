73a616c6599de2653f697c9eaf82f21e361db86d
JocelynAraya <arayameza.jocelyn@gmail.com>
2025-02-26 14:42:02 -0300

    fetch-products

--- Frontend/src/assets/hooks/useFetchProducts.jsx
+++ Frontend/src/assets/hooks/useFetchProducts.jsx
@@ -1,16 +1,21 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { getProducts } from "../../mockproducts";
+import { UserContext } from "../../context/UserContext";
 const useFetchProducts = () => {
   const [products, setProducts] = useState([]);
+  const {user} = useContext(UserContext)
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
         // const response = await getProducts()
-
-        const response = await axios.get("http://localhost:3000/api/productos");
+        const response = await axios.get("http://localhost:3000/api/productos/mis-productos", {
+          headers: { Authorization: `Bearer ${user.token}` }
+        }
+        );
         setProducts(response.data);
+        console.log(response.data);
         // console.log(response.data)
         // setProducts(response)
         
@@ -19,7 +24,7 @@ const useFetchProducts = () => {
       }
     };
     fetchProducts();
-  }, []);
+  }, [user.token]);
 
   return products ;
 };
